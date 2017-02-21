842dce98ea7f2fa9903293265785cb69af6745f1
Hunan Rostomyan <hunan131@gmail.com>
2017-02-20 20:11:11 -0800

    Add patch-instructions pass

--- src/r1_compile.rkt
+++ src/r1_compile.rkt
@@ -218,3 +218,29 @@
     [`(program ,varlist ,instructions ... ,ret)
      (let ([deref (map-to-stack varlist)])
        `(program ,(length deref) ,(assign-homes-aux instructions '() deref) ,ret))]))
+
+
+; Patch-instructions takes a pseudo-x86 program and transforms into an abstract x86
+; program by making sure each instruction references at most a single memory location.
+; x86* -> x86
+(define (patch-instructions-aux-instr instr)
+  (match instr
+    [`(,binop ,arg1 ,arg2)
+     (let ([leftderef? (equal? (car arg1) 'deref)]
+           [rightderef? (equal? (car arg2) 'deref)])
+       (if (and leftderef? rightderef?)
+           (list
+            `(movq ,arg1 (reg rax))
+            `(,binop (reg rax) ,arg2))
+           (list instr)))]
+    [else (list instr)]))
+
+(define (patch-instructions-aux instr new)
+  (if (empty? instr) new
+      (patch-instructions-aux (cdr instr) (append new (patch-instructions-aux-instr (car instr))))))
+
+(define (patch-instructions exp)
+  (match exp
+    [`(program ,framesize ,instructions ... ,ret)
+     ; taking car of instructions because we've been wrapping those in a list
+     `(program ,framesize ,(patch-instructions-aux (car instructions) '()) ,ret)]))
