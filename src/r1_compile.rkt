332a4185ccf440fc67e9dd67f59e72e05aa68679
Hunan Rostomyan <hunan131@gmail.com>
2017-02-20 19:40:04 -0800

    Add assign-homes pass

--- src/r1_compile.rkt
+++ src/r1_compile.rkt
@@ -182,6 +182,35 @@
 (define (select-instructions exp)
   (match exp
     [`(program ,varlist ,statements ... ,return)
-     `(program ,@(sel-instr-aux statements '()) (movq (var ,(cadr return)) (reg rax)) (retq))]))
      `(program ,varlist ,@(sel-instr-aux statements '()) (movq (var ,(cadr return)) (reg rax)) (retq))]))
 
+
+; Assign-homes takes a pseudo-x86 program with var arguments and places all of the variables
+; on the stack. Its output is still in pseudo-x86.
+; x86* -> x86*
+(define (assign-homes-aux-instr instr deref)
+  (match instr
+    [`(callq ,label) `(callq ,label)]
+    [`(,unop ,arg)
+     (list `(,unop ,(if (eq? (car arg) 'var)
+                        `(deref rbp ,(lookup deref (cadr arg)))
+                        arg)))]
+    [`(,binop ,arg1 ,arg2)
+     (list `(,binop ,(if (eq? (car arg1) 'var)
+                         `(deref rbp ,(lookup deref (cadr arg1)))
+                         arg1)
+                    ,(if (eq? (car arg2) 'var)
+                         `(deref rbp ,(lookup deref (cadr arg2)))
+                         arg2)))]
+    [else (list `(fuckoff))]))
+
+(define (assign-homes-aux instr new deref)
+  (if (empty? instr) new
+      (assign-homes-aux (cdr instr) (append new (assign-homes-aux-instr (car instr) deref)) deref)))
+
+
+(define (assign-homes exp)
+  (match exp
+    [`(program ,varlist ,instructions ... ,ret)
+     (let ([deref (map-to-stack varlist)])
+       (assign-homes-aux instructions '() deref))]))
