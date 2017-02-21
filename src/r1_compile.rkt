6510c5480be8ff4ce3eab9b1fac79e86458b5a18
Hunan Rostomyan <hunan131@gmail.com>
2017-02-20 19:39:03 -0800

    Add map-to-stack helper

--- src/r1_compile.rkt
+++ src/r1_compile.rkt
@@ -22,9 +22,23 @@
             (cdr head)
             (lookup rest var)))))
 
+
 ; Adds x to lst unless it's already in it.
 (define (add-unique x lst)
   (if (memv x lst) lst (cons x lst)))
+
+
+; Sorts list of symbols, assigning them to unique locations
+; on the stack.
+(define (map-to-stack-aux varlist cur env)
+  (if (empty? varlist) env
+      (let ([first (car varlist)][rest (cdr varlist)])
+        (map-to-stack-aux rest (+ -8 cur) (extend-env env first cur)))))
+
+(define (map-to-stack varlist)
+  (let ([sorted (sort varlist symbol<?)])
+    (map-to-stack-aux sorted -8 (new-env))))
+
 
 ; An interpreter for R1
 ; ---------------------
