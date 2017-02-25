ece45d9abc7311a667faddb3bd57b4cb84025460
Hunan Rostomyan <hunan131@gmail.com>
2017-02-25 15:44:03 -0800

    Fix runtime.c spacing

--- src/runtime.c
+++ src/runtime.c
@@ -2,12 +2,12 @@
 
 /* Writes the contents of RDI to stdout. */
 extern void print_int(int n) {
-	printf("%d\n", n);
+  printf("%d\n", n);
 }
 
-/* Read the contents of stdin onto the stack. */
+/* Reads the contents of stdin onto the stack. */
 extern int read_int() {
-	int i;
-	scanf("%d", &i);
-	return i;
+  int i;
+  scanf("%d", &i);
+  return i;
 }
