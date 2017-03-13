7e99171f4903094195b81dbc0201a2fca72a705b
Hunan Rostomyan <hunan131@gmail.com>
2017-03-12 23:11:19 -0700

    Add a case of shadowing

--- src/r1/compiler/uniquify-test.rkt
+++ src/r1/compiler/uniquify-test.rkt
@@ -7,6 +7,11 @@
   (let ([new (format "~a.~a" 'x (- ord 1))])
     (check-equal? output `(program (let ([,new 1]) ,new)))))
 
+(let ([output (uniquify `(program (let ([x 1]) (let ([x 2]) (+ x x)))))])
+  (let ([new1 (format "~a.~a" 'x (- ord 2))])
+    (let ([new2 (format "~a.~a" 'x (- ord 1))])
+      (check-equal? output `(program (let ([,new1 1]) (let ([,new2 2]) (+ ,new2 ,new2))))))))
+
 (let ([output (uniquify `(program (let ([x 1]) (let ([y 2]) (+ x y)))))])
   (let ([new1 (format "~a.~a" 'x (- ord 2))])
     (let ([new2 (format "~a.~a" 'y (- ord 1))])
