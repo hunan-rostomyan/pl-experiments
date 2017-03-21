afc77168583b4df13e8aa6d375857518ef4ef770
Hunan Rostomyan <hunan131@gmail.com>
2017-03-21 09:40:16 +0900

    Improve flatten tests

--- src/r1/compiler/flatten-test.rkt
+++ src/r1/compiler/flatten-test.rkt
@@ -1,5 +1,25 @@
 #lang racket/base
 
 (require rackunit "flatten.rkt")
+(require "../../util/uniqsym.rkt")
 
-(check-equal? (flatten `(program 5)) `(program () (return 5)))
+(define (genvar ord)
+  (string->symbol (format "t.~a" ord)))
+
+(check-equal?
+ (flatten `(program 5))
+ `(program () (return 5)))
+
+(let ([t0 (genvar ord)][t1 (genvar (+ ord 1))])
+  (check-equal?
+   (flatten `(program (- (read))))
+   `(program
+     (,t1 ,t0)
+     (assign ,t0 (read))
+     (assign ,t1 (- ,t0))
+     (return ,t1))))
+
+(let ([t0 (genvar ord)])
+  (check-equal?
+   (flatten `(program (- 5)))
+   `(program (,t0) (assign ,t0 (- 5)) (return ,t0))))
